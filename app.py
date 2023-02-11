19742e6fd700824aeaa7a698a829c8345b3e8aa8
Bijak <97365989+bijak@users.noreply.github.com>
2023-02-10 22:57:10 -0500

    Fixing requirements

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-import plotly.graph_objects as go
 
 import pandas as pd
 
