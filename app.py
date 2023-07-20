e94fdb65c909c7817d26b65fafc9b50810cb9b3c
bijak <97365989+bijak@users.noreply.github.com>
2023-07-20 04:54:50 -0400

    Adding a cumulative reviews graph

--- app.py
+++ app.py
@@ -17,13 +17,15 @@ pd.options.plotting.backend = "plotly"
 template = "plotly_dark"
 colors = {"background": "#111111", "text": "#ffffff"}
 default_fig = {}
-default_df = pd.DataFrame(columns=["Word", "Total Reviews", "Time to Learn", "No. Relapses", "Abandoned"])
-default_table = default_df.to_dict('records')
+default_df = pd.DataFrame(
+    columns=["Word", "Total Reviews", "Time to Learn", "No. Relapses", "Abandoned"]
+)
+default_table = default_df.to_dict("records")
 
-default_tz = 'America/New_York'
+default_tz = "America/New_York"
 timezone_list = []
 for tz in pytz.common_timezones:
-    timezone_list.append({'label':tz, 'value':tz})
+    timezone_list.append({"label": tz, "value": tz})
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
@@ -34,41 +36,49 @@ app.layout = dbc.Container(
         dbc.Modal(
             [
                 dbc.ModalHeader("Welcome to the Unofficial JPDB Stats Site!"),
-                dbc.ModalBody([
-                    dbc.Label("Select your time zone:"),
-                    dcc.Dropdown(
-                        id='timezone-dropdown',
-                        options=timezone_list,
-                        value=default_tz,
-                        style={'color': 'black'},
-                        persistence=True
-                    ),
-                    dbc.Label("Upload your review history:"),
-                    dcc.Upload(
-                        id="upload-data",
-                        children=html.Div(["This should be the reviews.json downloaded from the JPDB Settings page. Drag and Drop or ", html.A("Select File")]),
-                        style={
-                            "width": "100%",
-                            "height": "60px",
-                            "lineHeight": "60px",
-                            "borderWidth": "1px",
-                            "borderStyle": "dashed",
-                            "borderRadius": "5px",
-                            "textAlign": "center",
-                            "margin": "2px",
-                        },
-                        # Allow multiple files to be uploaded
-                        multiple=False,
-                    )
-                ])
+                dbc.ModalBody(
+                    [
+                        dbc.Label("Select your time zone:"),
+                        dcc.Dropdown(
+                            id="timezone-dropdown",
+                            options=timezone_list,
+                            value=default_tz,
+                            style={"color": "black"},
+                            persistence=True,
+                        ),
+                        dbc.Label("Upload your review history:"),
+                        dcc.Upload(
+                            id="upload-data",
+                            children=html.Div(
+                                [
+                                    "This should be the reviews.json downloaded from the JPDB Settings page. Drag and Drop or ",
+                                    html.A("Select File"),
+                                ]
+                            ),
+                            style={
+                                "width": "100%",
+                                "height": "60px",
+                                "lineHeight": "60px",
+                                "borderWidth": "1px",
+                                "borderStyle": "dashed",
+                                "borderRadius": "5px",
+                                "textAlign": "center",
+                                "margin": "2px",
+                            },
+                            # Allow multiple files to be uploaded
+                            multiple=False,
+                        ),
+                    ]
+                ),
             ],
             id="modal-upload",
             size="xl",
-            is_open=True
+            is_open=True,
         ),
-        dcc.Store(id='timezone'),
+        dcc.Store(id="timezone"),
         html.H3("Overall"),
         dcc.Graph(id="overall", figure=default_fig),
+        dcc.Graph(id="overall_cum", figure=default_fig),
         html.H3("New Cards"),
         dcc.Graph(id="new_cum", figure=default_fig),
         dcc.Graph(id="new_daily", figure=default_fig),
@@ -83,67 +93,138 @@ app.layout = dbc.Container(
         dcc.Graph(id="ret_learning", figure=default_fig),
         html.H3("Problem words"),
         dash_table.DataTable(
-            id='datatable-struggles',
-            columns=[
-                {"name": i, "id": i} for i in default_df.columns
-            ],
+            id="datatable-struggles",
+            columns=[{"name": i, "id": i} for i in default_df.columns],
             data=default_table,
             sort_action="native",
             page_action="native",
-            page_current= 0,
-            page_size= 100,
+            page_current=0,
+            page_size=100,
             export_format="csv",
-            style_header={
-                'backgroundColor': 'rgb(30, 30, 30)',
-                'color': 'white'
-            },
-            style_data={
-                'backgroundColor': 'rgb(50, 50, 50)',
-                'color': 'white'
-            }
+            style_header={"backgroundColor": "rgb(30, 30, 30)", "color": "white"},
+            style_data={"backgroundColor": "rgb(50, 50, 50)", "color": "white"},
         ),
-        html.Div(id='datatable-struggles-container')
+        html.Div(id="datatable-struggles-container"),
     ],
-    style={'backgroundColor':colors["background"]},
-    fluid=True
+    style={"backgroundColor": colors["background"]},
+    fluid=True,
 )
 
 
 @app.callback(
-    [Output("new_cum", "figure"),Output("new_daily", "figure"),Output("rev_cum", "figure"),Output("rev_daily", "figure"),Output("time_cum", "figure"),\
-        Output("time_daily", "figure"),Output("overall", "figure"),Output("datatable-struggles","data"),Output("ret_known","figure"),Output("ret_learning","figure")],
-    [Input("upload-data", "contents"), Input("upload-data", "filename"), Input("timezone", "data")],
+    [
+        Output("new_cum", "figure"),
+        Output("new_daily", "figure"),
+        Output("rev_cum", "figure"),
+        Output("rev_daily", "figure"),
+        Output("time_cum", "figure"),
+        Output("time_daily", "figure"),
+        Output("overall", "figure"),
+        Output("datatable-struggles", "data"),
+        Output("ret_known", "figure"),
+        Output("ret_learning", "figure"),
+        Output("overall_cum", "figure"),
+    ],
+    [
+        Input("upload-data", "contents"),
+        Input("upload-data", "filename"),
+        Input("timezone", "data"),
+    ],
 )
 def update_graph(contents, filename, timezone):
-    f_nc = default_fig; f_nd = default_fig; f_rc = default_fig; f_rd = default_fig; fm_rc = default_fig; fm_rd = default_fig
-    f_overall = default_fig; datatable = default_table; f_known = default_fig; f_learn = default_fig
-    if (filename is not None):
-        contents = contents.split(',')
+    f_nc = default_fig
+    f_nd = default_fig
+    f_rc = default_fig
+    f_rd = default_fig
+    fm_rc = default_fig
+    fm_rd = default_fig
+    f_overall = default_fig
+    datatable = default_table
+    f_known = default_fig
+    f_learn = default_fig
+    f_overall_cum = default_fig
+    if filename is not None:
+        contents = contents.split(",")
         contents = contents[1]
         new, rev, history, struggles = parse.parse_data(contents, timezone)
         f_rc, f_rd, fm_rc, fm_rd = plot.plot_reps(rev)
         f_nc, f_nd = plot.plot_new(new)
-        f_overall = plot.plot_history(history)
+        f_overall, f_overall_cum = plot.plot_history(history)
         f_known, f_learn = plot.plot_retention(history)
-        datatable = pd.DataFrame(struggles, columns=["Word", "Total Reviews", "Time to Learn", "No. Relapses", "Abandoned"]).to_dict('records')
+        datatable = pd.DataFrame(
+            struggles,
+            columns=[
+                "Word",
+                "Total Reviews",
+                "Time to Learn",
+                "No. Relapses",
+                "Abandoned",
+            ],
+        ).to_dict("records")
 
-    return [f_nc, f_nd, f_rc, f_rd, fm_rc, fm_rd, f_overall, datatable, f_known, f_learn]
+    return [
+        f_nc,
+        f_nd,
+        f_rc,
+        f_rd,
+        fm_rc,
+        fm_rd,
+        f_overall,
+        datatable,
+        f_known,
+        f_learn,
+        f_overall_cum,
+    ]
+
 
 @app.callback(
-    [Output("new_cum", "style"),Output("new_daily", "style"),Output("rev_cum", "style"),Output("rev_daily", "style"),Output("time_cum", "style"),\
-        Output("time_daily", "style"),Output("overall", "style"),Output("ret_known","style"),Output("ret_learning","style")],
+    [
+        Output("new_cum", "style"),
+        Output("new_daily", "style"),
+        Output("rev_cum", "style"),
+        Output("rev_daily", "style"),
+        Output("time_cum", "style"),
+        Output("time_daily", "style"),
+        Output("overall", "style"),
+        Output("ret_known", "style"),
+        Output("ret_learning", "style"),
+        Output("overall_cum", "style"),
+    ],
     [Input("upload-data", "filename")],
 )
 def update_display(filename):
-    if (filename is not None):
-        return [{"display":"block"},{"display":"block"},{"display":"block"},{"display":"block"},{"display":"block"},{"display":"block"},{"display":"block"},{"display":"block"},{"display":"block"}]
+    if filename is not None:
+        return [
+            {"display": "block"},
+            {"display": "block"},
+            {"display": "block"},
+            {"display": "block"},
+            {"display": "block"},
+            {"display": "block"},
+            {"display": "block"},
+            {"display": "block"},
+            {"display": "block"},
+            {"display": "block"},
+        ]
     else:
-        return [{"display":"none"},{"display":"none"},{"display":"none"},{"display":"none"},{"display":"none"},{"display":"none"},{"display":"none"},{"display":"none"},{"display":"none"}]
+        return [
+            {"display": "none"},
+            {"display": "none"},
+            {"display": "none"},
+            {"display": "none"},
+            {"display": "none"},
+            {"display": "none"},
+            {"display": "none"},
+            {"display": "none"},
+            {"display": "none"},
+            {"display": "none"},
+        ]
 
-@app.callback(Output("timezone","data"), [Input("timezone-dropdown","value")])
+
+@app.callback(Output("timezone", "data"), [Input("timezone-dropdown", "value")])
 def update_timezone(value):
-    dash.callback_context.response.set_cookie('jpdb-stats-tz', value)
     return value
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- plot.py
+++ plot.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import plotly.graph_objects as go
 
-def gen_cum_daily_plot(dataframe, type, unit, rolling=False, template='plotly_dark'):
+
+def gen_cum_daily_plot(dataframe, type, unit, rolling=False, template="plotly_dark"):
     idx = pd.date_range(dataframe.index.min(), dataframe.index.max())
     dataframe.index = pd.DatetimeIndex(dataframe.index)
     dataframe = dataframe.reindex(idx, fill_value=0)
@@ -12,97 +13,125 @@ def gen_cum_daily_plot(dataframe, type, unit, rolling=False, template='plotly_da
         title=f"{type} (Cum.)",
         yaxis_title=f"Total {unit}",
         xaxis_title="Date",
-        template=template
+        template=template,
     )
     # Daily Plot
     f_daily = dataframe.plot(kind="bar")
     if rolling:
         rolling_df = dataframe.rolling(7).mean()
         f_daily.add_trace(
-        go.Scatter(
-            name="Rolling Average",
-            x=rolling_df.index,
-            y=rolling_df["Count"]
-        ))
+            go.Scatter(
+                name="Rolling Average", x=rolling_df.index, y=rolling_df["Count"]
+            )
+        )
     f_daily.update_layout(
         title=f"{type} (Daily)",
         yaxis_title=f"Daily {unit}",
         xaxis_title="Date",
-        template=template
+        template=template,
     )
     return f_cum, f_daily
 
-def gen_retention_plot(dataframe, type, template='plotly_dark'):
+
+def gen_retention_plot(dataframe, type, template="plotly_dark"):
     idx = pd.date_range(dataframe.index.min(), dataframe.index.max())
     dataframe.index = pd.DatetimeIndex(dataframe.index)
     dataframe = dataframe.reindex(idx, fill_value=0)
-    dataframe['Retention'] = 100 * dataframe['Passed'] / (dataframe['Passed'] + dataframe['Failed'])
+    dataframe["Retention"] = (
+        100 * dataframe["Passed"] / (dataframe["Passed"] + dataframe["Failed"])
+    )
     window_df = dataframe.rolling(7).sum()
-    dataframe['Rolling Average'] = 100 * window_df['Passed'] / (window_df['Passed'] + window_df['Failed'])
-    f_retention = dataframe.plot(y='Retention', kind='bar')
+    dataframe["Rolling Average"] = (
+        100 * window_df["Passed"] / (window_df["Passed"] + window_df["Failed"])
+    )
+    f_retention = dataframe.plot(y="Retention", kind="bar")
     f_retention.add_trace(
         go.Scatter(
-            name="Rolling Average",
-            x=dataframe.index,
-            y=dataframe["Rolling Average"]
-        ))
+            name="Rolling Average", x=dataframe.index, y=dataframe["Rolling Average"]
+        )
+    )
     f_retention.update_layout(
         title=f"Retention ({type})",
         yaxis_title="Retention (%)",
         xaxis_title="Date",
-        template=template
+        template=template,
     )
     return f_retention
 
-def plot_reps(rep, template='plotly_dark'):
-    reviews = pd.DataFrame(rep, columns=['Date'])
-    reviews['Count'] = 1
-    reviews_minute = pd.DataFrame(rep, columns=['Date'])
+
+def plot_reps(rep, template="plotly_dark"):
+    reviews = pd.DataFrame(rep, columns=["Date"])
+    reviews["Count"] = 1
+    reviews_minute = pd.DataFrame(rep, columns=["Date"])
     reviews_minute["Count"] = 1
-    reviews = reviews.groupby(pd.Grouper(key="Date", freq='1D')).sum()
-    reviews_minute = reviews_minute.groupby(pd.Grouper(key="Date", freq='1Min')).sum()
+    reviews = reviews.groupby(pd.Grouper(key="Date", freq="1D")).sum()
+    reviews_minute = reviews_minute.groupby(pd.Grouper(key="Date", freq="1Min")).sum()
 
     f_rc, f_rd = gen_cum_daily_plot(reviews, "Repetitions", "repetitions", rolling=True)
     # Time spent
     reviews_minute.index = pd.DatetimeIndex(reviews_minute.index)
-    reviews_minute["Count"] = reviews_minute['Count'].where(reviews_minute['Count'] == 0, 1)
-    reviews_minute = reviews_minute.groupby(pd.Grouper(freq='D')).sum()
-    
-    fm_rc, fm_rd = gen_cum_daily_plot(reviews_minute, "Time Spent", "time (minutes)", rolling=True)
-    
+    reviews_minute["Count"] = reviews_minute["Count"].where(
+        reviews_minute["Count"] == 0, 1
+    )
+    reviews_minute = reviews_minute.groupby(pd.Grouper(freq="D")).sum()
+
+    fm_rc, fm_rd = gen_cum_daily_plot(
+        reviews_minute, "Time Spent", "time (minutes)", rolling=True
+    )
 
     return f_rc, f_rd, fm_rc, fm_rd
 
-def plot_history(history, template='plotly_dark'):
-    history_df = (pd.DataFrame(history, columns=['Date', 'Failed', 'Passed', 'New', 'Abandoned', 'Known'])).drop(['Known'], axis=1)
-    history_df = history_df.groupby('Date').sum()
+
+def plot_history(history, template="plotly_dark"):
+    history_df = (
+        pd.DataFrame(
+            history, columns=["Date", "Failed", "Passed", "New", "Abandoned", "Known"]
+        )
+    ).drop(["Known"], axis=1)
+    history_df = history_df.groupby("Date").sum()
     idx = pd.date_range(history_df.index.min(), history_df.index.max())
     history_df.index = pd.DatetimeIndex(history_df.index)
     history_df = history_df.reindex(idx, fill_value=0)
-    colors = ['#EF553B','#00CC96','#636EFA', '#D3D3D3']
-    f_history = history_df.plot(kind='bar', color_discrete_sequence=colors)
+    colors = ["#EF553B", "#00CC96", "#636EFA", "#D3D3D3"]
+    f_history = history_df.plot(kind="bar", color_discrete_sequence=colors)
     f_history.update_layout(
         title="Cards (Daily)",
         yaxis_title="Card Count",
         xaxis_title="Date",
-        template=template
+        template=template,
     )
-    return f_history
+    history_df = history_df.cumsum()
+    f_history_cum = history_df.plot(kind="bar", color_discrete_sequence=colors)
+    f_history_cum.update_layout(
+        title="Cards (Cum.)",
+        yaxis_title="Card Count",
+        xaxis_title="Date",
+        template=template,
+    )
+    return f_history, f_history_cum
 
-def plot_retention(history, template='plotly_dark'):
-    retention_df = pd.DataFrame(history, columns=['Date', 'Failed', 'Passed', 'New', 'Abandoned', 'Known'])
-    retention_known_df = retention_df[retention_df['Known']==True].groupby('Date').sum()
-    retention_learning_df = retention_df[retention_df['Known']==False].groupby('Date').sum()
-    
-    known_plot = gen_retention_plot(retention_known_df, 'Known', template)
-    learning_plot = gen_retention_plot(retention_learning_df, 'Learning', template)
+
+def plot_retention(history, template="plotly_dark"):
+    retention_df = pd.DataFrame(
+        history, columns=["Date", "Failed", "Passed", "New", "Abandoned", "Known"]
+    )
+    retention_known_df = (
+        retention_df[retention_df["Known"] == True].groupby("Date").sum()
+    )
+    retention_learning_df = (
+        retention_df[retention_df["Known"] == False].groupby("Date").sum()
+    )
+
+    known_plot = gen_retention_plot(retention_known_df, "Known", template)
+    learning_plot = gen_retention_plot(retention_learning_df, "Learning", template)
 
     return known_plot, learning_plot
 
-def plot_new(new_in, template='plotly_dark'):
-    new = pd.DataFrame(new_in, columns=['Date'])
-    new['Count'] = 1
-    new = new.groupby('Date').sum()
+
+def plot_new(new_in, template="plotly_dark"):
+    new = pd.DataFrame(new_in, columns=["Date"])
+    new["Count"] = 1
+    new = new.groupby("Date").sum()
 
     f_nc, f_nd = gen_cum_daily_plot(new, "New Cards", "new cards added", rolling=True)
 
